36f1bc845154289e4d567327318666228fb621be
Mahmoud Elkafrawy@ELKAFRAWY <mahmoud.kafrawy96@gmail.com>
2022-09-26 15:36:59 +0200

    make products static pages

--- pages/product/[id]/index.js
+++ pages/product/[id]/index.js
@@ -21,7 +21,7 @@ const index = ({ product }) => {
   );
 };
 
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
   const res = await fetch(`https://fakestoreapi.com/products/${context.params.id}`);
   const product = await res.json();
 
@@ -32,12 +32,12 @@ export const getServerSideProps = async (context) => {
   };
 };
 
-// export const getStaticPaths = async () => {
-//   const res = await fetch(`https://fakestoreapi.com/products/`);
-//   const products = await res.json();
-//   const ids = products.map((product) => product.id);
-//   const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://fakestoreapi.com/products/`);
+  const products = await res.json();
+  const ids = products.map((product) => product.id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
 
-//   return { paths, fallback: false };
-// };
+  return { paths, fallback: false };
+};
 export default index;
